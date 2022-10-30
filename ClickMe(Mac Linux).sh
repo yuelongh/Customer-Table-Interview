87f1f9265523e6c5009195b7830dfb1efe56bcaa
yuelongh <41211865+yuelongh@users.noreply.github.com>
2022-10-30 14:36:21 +0000

    Update ClickMe(Mac Linux).sh

--- ClickMe(Mac Linux).sh
+++ ClickMe(Mac Linux).sh
@@ -1,7 +1,9 @@
-echo Have you installed Node.js and npm?
-pause
-read -e -p " Enter Port Number (Leave It Blank For Using Default Port 5000):" -i "5000" port
-echo Use Port $port
+read -p "Have you installed Node.js and npm? (Hit Enter to continue)"
+read -p "Enter Port Number (Leave It Blank For Using Default Port 5000): " port
+port=${port:-5000}
+echo $name
+echo It will use Port $port
 echo "$PWD"
-cd $PWD
-npm $port run use_click_me %port%
+cd We are in $PWD
+echo npm run use_click_me $port
+npm run use_click_me $port
